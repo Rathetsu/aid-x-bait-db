18acf8b6fb1c4a717874764871efb9dd02d4b6c8
Ahmed Ezzat <ah.ezzat.muh@gmail.com>
2025-01-18 21:25:19 +0200

    Added date and time_slot to the requests table

--- visits/tables/requests.sql
+++ visits/tables/requests.sql
@@ -7,6 +7,8 @@ CREATE TABLE visits.requests
     id         SERIAL PRIMARY KEY,
     patient_id INTEGER NOT NULL UNIQUE REFERENCES core.patients (id),
     location_id INTEGER NOT NULL UNIQUE REFERENCES core.locations (id),
+    request_date DATE NOT NULL,
+    time_slot VARCHAR(255) NOT NULL CHECK (time_slot IN ('8:00-12:00', '12:00-16:00', '16:00-20:00')),
     status     VARCHAR(255) NOT NULL CHECK (status IN ('Pending', 'Accepted', 'Rejected')),
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
